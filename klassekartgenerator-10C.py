41db48c92afc67509dc8da51c6b132635044520f
Raspeball <Raspeball@users.noreply.github.com>
2023-09-20 22:00:09 +0200

    Update klassekartgenerator-10C.py
    Trying to write to text

--- klassekartgenerator-10C.py
+++ klassekartgenerator-10C.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 # markdown
-import Mdutils
+import mdutils
 #
 #
 #
@@ -83,19 +83,71 @@ def MakeKlassekart(grupper, navnedic, kartdic):
 #
 # Generate seat map as markdown
 def GenerateMdFile(finalseatmap):
+    # data is finalseatmap
     # create file
-    mdFile = Mdutils(file_name = "klassekart-10C", title = "Klassekart 10C uke XX")
+    mdFile = mdutils(file_name ="klassekart_10C", title = "Klassekart 10C uke XX")
+    
+
+    # create tables
+    pos = finalseatmap.keys()
+    foran = pos[0:3]
+    #midten = pos[3:6]
+    #bak = [pos[-3], "bak midten", pos[-1]]
+
+    for pos in foran:
+        foran.extend([pos[0][0] + " " + pos[0][1], pos[1][0] + " " + pos[1][1], pos[2][0] + " " + pos[2][1]])
+    
+    mdFile.new_line()
+    mdFile.new_table(columns = 3, rows = 2, text = foran, text_align = "center")
     mdFile.create_md_file()
 
+# Generate seat map as text file
+def GenerateTxtFile(finalseatmap):
+    f = open("klasseklart_10C.txt", "a")
 
+    # create rows
+    pos = list(finalseatmap.keys())
+    foran = pos[0:3]
+    midten = pos[3:6]
+    bak = [pos[-3], "bak midten", pos[-1]]
 
+    rows = ["", "", ""]
 
+    for place, group in finalseatmap.items():
+        if place in pos:
+            if place in foran:
+                t = place+"\n"
+                for i in group:
+                    t += i + " "
+                t += "          "
+                rows[0] += t
+            
+            elif place in midten:
+                t = place+"\n"
+                for i in group:
+                    t += i + " "
+                t += "          "
+                rows[1] += t
+            
+            elif place in bak:
+                t = place+"\n"
+                for i in group:
+                    t += i + " "
+                t += "          "
+                rows[2] += t
+    
+    for row in rows:
+        f.write(row)
+    
+    f.close() 
+#
+#
+#
 # Testing #
 #test = MakeGrupper(Klasse10C())
 grupper = MakeGrupper(Klasse10C())
-test2 = MakeKlassekart(grupper, Klasse10C(), Seatmap())
-print(test2)
-
+klassekart = MakeKlassekart(grupper, Klasse10C(), Seatmap())
+GenerateTxtFile(klassekart)
     
 
 
